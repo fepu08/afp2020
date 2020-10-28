bdd41533481778c697d4a99601a6fe7c3a895967
Deák Ádám <sedeannary@gmail.com>
2020-10-28 15:08:15 +0100

    AquaparkController added

--- backend/src/main/java/hu/uni/eku/tzs/controller/AquaparkController.java
+++ backend/src/main/java/hu/uni/eku/tzs/controller/AquaparkController.java
@@ -1,4 +1,17 @@
 package hu.uni.eku.tzs.controller;
 
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/Aquapark")
+@RequiredArgsConstructor
+@Api(tags = "Aquapark")
+@Slf4j
 public class AquaparkController {
+
+
 }
