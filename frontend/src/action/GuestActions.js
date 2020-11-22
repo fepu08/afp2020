e9520c0caeb9395c76e9385e876928a3a7de9c4a
gergof2 <heroghost20001024@gmail.com>
2020-11-22 14:05:44 +0100

    fetchGuest létrehozva
    ref: #27

--- frontend/src/action/GuestActions.js
+++ frontend/src/action/GuestActions.js
@@ -2,7 +2,15 @@ import axios from 'axios';
 import dispatcher from '../dispatcher/Dispatcher';
 import * as actionConstants from '../dispatcher/GuestActionConstants'
 
-//TODO: getGuests
+//TODO: fetchGuests
+export const fetchGuest = () =>{
+    axios.get('/Guest/').then((resp)=>{
+        dispatcher.dispatch({
+            action : actionConstants.refresh,
+            payload: resp.data
+        });
+    })
+}
 //TODO: checkInGuests
 //TODO: getGuestById
 //TODO: deleteGuest
