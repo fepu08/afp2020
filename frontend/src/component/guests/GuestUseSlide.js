39da02d45ace615fc865cd81b6ca7e70eeaad073
fepu08 <besenyeiferenc@protonmail.com>
2020-11-23 14:12:50 +0100

    Modded GuestUseSlide use slide show alerts

--- frontend/src/component/guests/GuestUseSlide.js
+++ frontend/src/component/guests/GuestUseSlide.js
@@ -8,7 +8,8 @@ class GuestUseSlide extends React.Component{
         super(props);
         this.state = {
             slideId: 0,
-            watchId: this.props.watchId
+            watchId: this.props.watchId,
+            buttonPushed: false
         };
         this.formOnChange = this.formOnChange.bind(this);
     }
@@ -16,14 +17,19 @@ class GuestUseSlide extends React.Component{
     formOnChange(event){
         const {name,value} = event.target;
         this.setState({[name] : value});
+        this.setState({buttonPushed: false})
     }
     render() {
         return (
             <Row className={"form-group px-2"}>
-                <Col xs={"12"}>
-                    {/* azért így kell, hogy ne jelenjen meg minden vendégnél a listában a hiba*/}
-                    {(this.state.slideId !== 0) && <ErrorMessageWell/>}
-                </Col>
+                {
+                    (this.state.slideId !== 0 && this.state.buttonPushed) &&
+                    <Col xs={"12"} className={"mb-3"}>
+                        {/* azért így kell, hogy ne jelenjen meg minden vendégnél a listában a hiba*/}
+                        <ErrorMessageWell/>
+                    </Col>
+                }
+
 
                 <Col sm={4} lg={"2"}>
                     <label htmlFor={"slideId"} className={"col-form-label"}>Slide ID</label>
@@ -31,7 +37,10 @@ class GuestUseSlide extends React.Component{
                 <Col sm={8} lg={"10"}>
                     <input type="number" name="slideId" className={"form-control"} placeholder={"Insert Slide ID"} id={"slideId"} onChange={this.formOnChange}/>
                 </Col>
-                <button className={"btn btn-info btn-block mt-3"} onClick={() => actions.useSlide(this.state)}><strong>Slide!</strong></button>
+                <button className={"btn btn-info btn-block mt-3"} onClick={() => {
+                    actions.useSlide(this.state);
+                    this.setState({buttonPushed: true});
+                }}><strong>Slide!</strong></button>
             </Row>
         );
     }
