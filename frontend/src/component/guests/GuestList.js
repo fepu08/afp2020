42dd10c0b28154a8846d90af5eb76544a59650b2
fepu08 <besenyeiferenc@protonmail.com>
2020-11-23 13:10:46 +0100

    If guests list is empty then write it out
    ref #52

--- frontend/src/component/guests/GuestList.js
+++ frontend/src/component/guests/GuestList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {default as store} from '../../store/GuestStore';
 import GuestListItem from "./GuestListItem";
-import {Row} from "react-bootstrap";
+import {Row, Col} from "react-bootstrap";
 
 class GuestList extends React.Component{
     constructor(props) {
@@ -25,21 +25,34 @@ class GuestList extends React.Component{
     }
 
     render() {
-        return(
-            <Row className={"w-100"}>
-                {this.state.guests.map(({arrivalDateTime, id, transactions, watch}, index)=>{
-                    return(
-                        <GuestListItem
-                            key={index}
-                            arrivalDateTime={arrivalDateTime}
-                            id={id}
-                            transactions={transactions}
-                            watch={watch}
-                        />
-                    );
-                })}
-            </Row>
-        );
+        if(this.state.guests.length === 0){
+            return(
+                <Row className={"w-100"}>
+                    <Col xs={"12"}>
+                        <div className={"alert alert-warning"}>
+                            No Guests in the list
+                        </div>
+                    </Col>
+                </Row>
+            );
+        } else {
+            return(
+                <Row className={"w-100"}>
+                    {this.state.guests.map(({arrivalDateTime, id, transactions, watch}, index)=>{
+                        return(
+                            <GuestListItem
+                                key={index}
+                                arrivalDateTime={arrivalDateTime}
+                                id={id}
+                                transactions={transactions}
+                                watch={watch}
+                            />
+                        );
+                    })}
+                </Row>
+            );
+        }
+
     }
 
 }
