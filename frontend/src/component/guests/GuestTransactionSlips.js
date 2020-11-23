034186754e949c7ebbc8bf8b40aa5cb0fde27b10
Ferenc Besenyei <55558183+fepu08@users.noreply.github.com>
2020-11-23 09:13:26 +0100

    Made transaction list look better

--- frontend/src/component/guests/GuestTransactionSlips.js
+++ frontend/src/component/guests/GuestTransactionSlips.js
@@ -7,12 +7,15 @@ function GuestTransactionSlips({slips}){
             {slips.map(({id, guestId, slide, timestamp}, index) => {
                 return(
                     <Fragment key={index}>
+                        {console.log(index)}
+                        <hr/>
                         <li key={id}>ID: {id}</li>
                         <li key={guestId}>Guest ID: {guestId}</li>
                         <li key={guestId + "+" + guestId}>Slide:
                             <SlideListItemForGuestTransaction id={slide.id} price={slide.price} slideName={slide.slideName}/>
                         </li>
                         <li key={timestamp}>Timestamp: {timestamp}</li>
+                        {(index === (slips.length -1 ))&& <hr/>}
                     </Fragment>
                 );
             })}
